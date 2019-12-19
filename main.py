7cc948cc939894ecd762c44be80a99408f1194f2
Joshua Byrd <phocks@gmail.com>
2019-12-19 13:44:07 +1000

    Reformat

--- main.py
+++ main.py
@@ -19,20 +19,18 @@ def is_all_zero(dict):
 def get_temperature_dict(file, input_lat, input_lon):
     in_nc = nc.Dataset(file)
 
-    temps = in_nc.variables['HWD_EHF']
-    mt = in_nc.variables['time']  # read time variable
+    temps = in_nc.variables["HWD_EHF"]
+    mt = in_nc.variables["time"]  # read time variable
 
     time = mt[:]  # Reads the netCDF variable MT, array of one element
 
-    time_unit = in_nc.variables["time"].getncattr('units')
-    time_cal = in_nc.variables["time"].getncattr(
-        'calendar')  # read calendar type
-    local_time = nc.num2date(time, units=time_unit,
-                             calendar=time_cal)  # convert time
+    time_unit = in_nc.variables["time"].getncattr("units")
+    time_cal = in_nc.variables["time"].getncattr("calendar")  # read calendar type
+    local_time = nc.num2date(time, units=time_unit, calendar=time_cal)  # convert time
     # print("Original time %s is now converted as %s" %
     #       (time[0], local_time[0]))  # check conversion
 
-    lat, lon = in_nc.variables['lat'], in_nc.variables['lon']
+    lat, lon = in_nc.variables["lat"], in_nc.variables["lon"]
 
     target_lat = input_lat
     target_lon = input_lon
@@ -43,21 +41,20 @@ def get_temperature_dict(file, input_lat, input_lon):
     # Convert lat lon to position in 2D array
     def getclosest_ij(lats, lons, latpt, lonpt):
         # find squared distance of every point on grid
-        dist_sq = (lats-latpt)**2 + (lons-lonpt)**2
+        dist_sq = (lats - latpt) ** 2 + (lons - lonpt) ** 2
         minindex_flattened = dist_sq.argmin()  # 1D index of minimum dist_sq element
         # Get 2D index for latvals and lonvals arrays from 1D index
         return np.unravel_index(minindex_flattened, latvals.shape)
 
     iy_min, ix_min = getclosest_ij(latvals, lonvals, target_lat, target_lon)
-    #print(iy_min, ix_min)
+    # print(iy_min, ix_min)
 
     temps_vals = temps[:]
     temperature_dict = {}
 
     for year in range(len(local_time)):
-        #print(temps[year, iy_min, ix_min])
-        temperature_dict[str(local_time[year].year)] = int(
-            temps[year, iy_min, ix_min])
+        # print(temps[year, iy_min, ix_min])
+        temperature_dict[str(local_time[year].year)] = int(temps[year, iy_min, ix_min])
 
     return temperature_dict
 
@@ -66,19 +63,23 @@ def main_process(input_lat, input_lon):
     print(input_lat, input_lon)
 
     historical = get_temperature_dict(
-        "./data/CCRC_NARCliM_YEA_1950-2009_HWD_EHF_NF13.nc", input_lat, input_lon)
+        "./data/CCRC_NARCliM_YEA_1950-2009_HWD_EHF_NF13.nc", input_lat, input_lon
+    )
     modern = get_temperature_dict(
-        "./data/CCRC_NARCliM_YEA_1990-2009_HWD_EHF_NF13.nc", input_lat, input_lon)
+        "./data/CCRC_NARCliM_YEA_1990-2009_HWD_EHF_NF13.nc", input_lat, input_lon
+    )
     projection_1 = get_temperature_dict(
-        "./data/CCRC_NARCliM_YEA_2020-2039_HWD_EHF_NF13.nc", input_lat, input_lon)
+        "./data/CCRC_NARCliM_YEA_2020-2039_HWD_EHF_NF13.nc", input_lat, input_lon
+    )
     projection_2 = get_temperature_dict(
-        "./data/CCRC_NARCliM_YEA_2060-2079_HWD_EHF_NF13.nc", input_lat, input_lon)
+        "./data/CCRC_NARCliM_YEA_2060-2079_HWD_EHF_NF13.nc", input_lat, input_lon
+    )
 
     return_value = {
         "historical": historical,
         "modern": modern,
         "projection_1": projection_1,
-        "projection_2": projection_2
+        "projection_2": projection_2,
     }
 
     return return_value
@@ -121,10 +122,12 @@ def heatwave_api(request):
         final_return = main_process(scan_lat, scan_lon)
 
         # Check if still in the ocean
-        if (is_all_zero(final_return["historical"]) and
-            is_all_zero(final_return["modern"]) and
-            is_all_zero(final_return["projection_1"]) and
-                is_all_zero(final_return["projection_2"])):
+        if (
+            is_all_zero(final_return["historical"])
+            and is_all_zero(final_return["modern"])
+            and is_all_zero(final_return["projection_1"])
+            and is_all_zero(final_return["projection_2"])
+        ):
 
             #     print("Likely not on land... moving position towards middle Australia")
             #     if input_lat <= AUS_CENTER["lat"]:
@@ -157,10 +160,11 @@ def heatwave_api(request):
 
 if __name__ == "__main__":
     from flask import Flask, request
+
     app = Flask(__name__)
 
-    @app.route('/', methods=["GET", "POST"])
+    @app.route("/", methods=["GET", "POST"])
     def index():
         return heatwave_api(request)
 
-    app.run('127.0.0.1', 8000, debug=True)
+    app.run("127.0.0.1", 8000, debug=True)
